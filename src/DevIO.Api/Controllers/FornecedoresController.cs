7ddb25a35997b4e57856e954e0b3fee3030409d0
unknown <flaviofz@gmail.com>
2021-06-23 20:30:17 -0300

    Modelando a controller de Fornecedores

--- src/DevIO.Api/Controllers/FornecedoresController.cs
+++ src/DevIO.Api/Controllers/FornecedoresController.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using AutoMapper;
 using DevIO.Api.ViewModels;
 using DevIO.Business.Intefaces;
+using DevIO.Business.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DevIO.Api.Controllers
@@ -11,15 +13,17 @@ namespace DevIO.Api.Controllers
     public  class FornecedoresController : MainController
     {
         private readonly IFornecedorRepository _fornecedorRespository;
+        private readonly IFornecedorService _fornecedorService;
         private readonly IMapper _mapper;
 
         public FornecedoresController(
-            IFornecedorRepository fornecedorRespository, 
-            IMapper mapper
-        )
+            IFornecedorRepository fornecedorRespository,
+            IMapper mapper, 
+            IFornecedorService fornecedorService)
         {
             _fornecedorRespository = fornecedorRespository;
             _mapper = mapper;
+            _fornecedorService = fornecedorService;
         }
 
         [HttpGet]
@@ -28,6 +32,68 @@ namespace DevIO.Api.Controllers
             var fornecedores = _mapper.Map<IEnumerable<FornecedorViewModel>>(await _fornecedorRespository.ObterTodos());
 
             return Ok(fornecedores);
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<IEnumerable<FornecedorViewModel>>> ObterPorId(Guid id)
+        {
+            var fornecedor = await FornecedorProdutosEndereco(id);
+
+            if (fornecedor == null) return NotFound();
+
+            return Ok(fornecedor);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<FornecedorViewModel>> Adicionar(FornecedorViewModel fornecedorViewModel)
+        {
+            if(!ModelState.IsValid) return BadRequest();
+
+            var fornecedor = _mapper.Map<Fornecedor>(fornecedorViewModel);
+            var result = await _fornecedorService.Adicionar(fornecedor);
+
+            if(!result) return BadRequest();
+
+            return Ok();
+        }
+
+        [HttpPut("{id:guid}")] // A partir do 2.1 já entende que está recebendo da rota e do body
+        public async Task<ActionResult<FornecedorViewModel>> Atualizar(Guid id, FornecedorViewModel fornecedorViewModel)
+        {
+            if (id != fornecedorViewModel.Id) return BadRequest();
+
+            if(!ModelState.IsValid) return BadRequest();
+
+            var fornecedor = _mapper.Map<Fornecedor>(fornecedorViewModel);
+            var result  = await _fornecedorService.Atualizar(fornecedor);
+
+            if(!result) return BadRequest();
+
+            return Ok();
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult<FornecedorViewModel>> Excluir(Guid id)
+        {
+            var fornecedor = await FornecedorEndereco(id);
+
+            if (fornecedor == null) return NotFound();
+
+            var result = await _fornecedorService.Remover(id);
+
+            if (!result) return BadRequest();
+
+            return Ok();
+        }
+
+        private async Task<FornecedorViewModel> FornecedorProdutosEndereco(Guid id)
+        {
+            return _mapper.Map<FornecedorViewModel>(await _fornecedorRespository.ObterFornecedorProdutosEndereco(id));
+        }
+
+        private async Task<FornecedorViewModel> FornecedorEndereco(Guid id)
+        {
+            return _mapper.Map<FornecedorViewModel>(await _fornecedorRespository.ObterFornecedorEndereco(id));
         }
     }
 }
--- src/DevIO.Business/Models/Validations/FornecedorValidation.cs
+++ src/DevIO.Business/Models/Validations/FornecedorValidation.cs
@@ -8,9 +8,10 @@ namespace DevIO.Business.Models.Validations
         public FornecedorValidation()
         {
             RuleFor(f => f.Nome)
-                .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
+                .NotEmpty()
+                    .WithMessage("O campo {PropertyName} precisa ser fornecido")
                 .Length(2, 100)
-                .WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres");
+                    .WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres");
 
             When(f => f.TipoFornecedor == TipoFornecedor.PessoaFisica, () =>
             {
